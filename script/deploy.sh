1a2d2562e46c8403917a5d7cdfa787bbbe900627
liuzhengyang <liuzhengyang75@gmail.com>
2018-12-30 15:52:42 +0800

    fix deploy script

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-git pull
+jps -lvm | grep grpc-swagger-web | awk -F ' ' '{print $1}' | xargs kill -9
 
-jps -lvm | grep grpc-web | awk -F ' ' '{print $1}' | xargs kill -9
-
-./mvnw -pl grpc-web -am clean install
+./mvnw -pl grpc-swagger-web -am clean install
 
 java -jar grpc-swagger-web/target/grpc-swagger-web-0.0.1-SNAPSHOT.jar --spring.profiles.active=production
