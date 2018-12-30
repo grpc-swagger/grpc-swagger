c508a0fbae5290a7852001588705e5f2437be729
liuzhengyang <liuzhengyang75@gmail.com>
2018-12-30 15:40:01 +0800

    add deploy script

--- script/deploy.sh
+++ script/deploy.sh
@@ -6,4 +6,4 @@ jps -lvm | grep grpc-web | awk -F ' ' '{print $1}' | xargs kill -9
 
 ./mvnw -pl grpc-web -am clean install
 
-nohup java -jar grpc-web/target/grpc-web-0.0.1-SNAPSHOT.jar --spring.profiles.active=production &
+java -jar grpc-web/target/grpc-web-0.0.1-SNAPSHOT.jar --spring.profiles.active=production
