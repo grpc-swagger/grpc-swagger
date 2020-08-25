2bfa9e73fb40c6fdbf5b6f808adda1b4f101c84c
TommyLemon <1184482681@qq.com>
2020-08-26 01:17:43 +0800

    支持前端 JavaScript 通过 CORS 跨域

--- grpc-swagger-web/src/main/java/io/grpc/grpcswagger/GrpcSwaggerApplication.java
+++ grpc-swagger-web/src/main/java/io/grpc/grpcswagger/GrpcSwaggerApplication.java
@@ -3,7 +3,13 @@ package io.grpc.grpcswagger;
 import org.apache.log4j.BasicConfigurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
+@Configuration
 @SpringBootApplication(scanBasePackages = "io.grpc.grpcswagger")
 public class GrpcSwaggerApplication {
 
@@ -11,4 +17,30 @@ public class GrpcSwaggerApplication {
         BasicConfigurator.configure();
         SpringApplication.run(GrpcSwaggerApplication.class, args);
     }
+
+
+    //支持JavaScript跨域请求<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    /**
+     * 跨域过滤器
+     * @return
+     */
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig());
+        return new CorsFilter(source);
+    }
+    /**CORS跨域配置
+     * @return
+     */
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*"); //允许的域名或IP地址
+        corsConfiguration.addAllowedHeader("*"); //允许的请求头
+        corsConfiguration.addAllowedMethod("*"); //允许的HTTP请求方法
+        corsConfiguration.setAllowCredentials(true); //允许发送跨域凭据，前端Axios存取JSESSIONID必须要
+        return corsConfiguration;
+    }
+    //支持JavaScript跨域请求 >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
 }
